90ff347fe9b1f7a42e27a716d07b41a462bc051c
Volodymyr Halchuk <volodymyr.halchuk.dev@gmail.com>
2023-10-07 14:38:01 +0400

    Add header

---
+++ components/Header.js
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Header() {
+  return (
+    <View style={{ padding: 16 }}>
+      <Text>Little Lemon Restaurant</Text>
+    </View>
+  );
+}
